5042e724a8ed169775dfca9c65106e630d08fb64
Vishnu <vishnubishnoi7@gmail.com>
2024-11-22 00:14:53 +0530

    Move unused pubic protocols internal to SDK

--- Sources/JoyfillModel/DocumentModel.swift
+++ Sources/JoyfillModel/DocumentModel.swift
@@ -479,23 +479,6 @@ public protocol FormInterface {
     var events: FormChangeEvent? { get set }
 }
 
-/// `FieldChangeEvent` is a structure that encapsulates the changes in a field.
-///
-/// It contains information about the position of the field, the field itself, the page containing the field, and the file associated with the field.
-public struct FieldChangeEvent {
-    public let fieldPosition: FieldPosition
-    public let field: JoyDocField?
-    public var page: Page?
-    public var file: File?
-    
-    public init(fieldPosition: FieldPosition, field: JoyDocField?, page: Page? = nil, file: File? = nil) {
-        self.fieldPosition = fieldPosition
-        self.field = field
-        self.page = page
-        self.file = file
-    }
-}
-
 /// A struct representing a change in a document.
 public struct Change {
     /// The dictionary representation of the change.
@@ -632,68 +615,6 @@ public protocol FormChangeEvent {
     /// (params: object) => {} :
     /// - Specifies information about the uploaded file.
     func onUpload(event:UploadEvent)
-}
-
-/// `FormChangeEventInternal` is a protocol that defines the methods to handle form change events.
-public protocol FormChangeEventInternal {
-    
-    /// A method that is called when a field's value changes.
-    ///
-    /// - Parameter event: The `FieldChangeEvent` object that contains information about the field change event.
-    func onChange(event: FieldChangeEvent)
-    
-    /// Adds a row to the form with the specified field change event.
-    ///
-    /// - Parameters:
-    ///   - event: The field change event containing the necessary information for adding a row.
-    func addRow(event: FieldChangeEvent, targetRowIndexes: [TargetRowModel])
-
-    func moveRow(event: FieldChangeEvent, targetRowIndexes: [TargetRowModel])
-
-    func deleteRow(event: FieldChangeEvent, targetRowIndexes: [TargetRowModel])
-
-    /// Notifies the form view that it has received focus.
-    ///
-    /// - Parameter event: The field event associated with the focus.
-    func onFocus(event: FieldEvent)
-    
-    /// Calls the `onBlur` event handler with the specified `event`.
-    ///
-    /// - Parameter event: The `FieldEvent` to pass to the `onBlur` event handler.
-    func onBlur(event: FieldEvent)
-    
-    /// Calls the `onUpload` method of the `events` object, passing the provided `event`.
-    ///
-    /// - Parameter event: The `UploadEvent` to be passed to the `onUpload` method.
-    func onUpload(event:UploadEvent)
-}
-
-/// A protocol that defines the field change events for a document.
-public protocol FieldChangeEvents {
-    
-    /// Notifies the conforming object when a field change event occurs.
-    ///
-    /// - Parameter event: The `FieldChangeEvent` object that represents the field change event.
-    func onChange(event: FieldChangeEvent)
-    
-    /// Adds a new row to the document when a field change event occurs.
-    ///
-    /// - Parameter event: The `FieldChangeEvent` object that represents the field change event.
-    func addRow(event: FieldChangeEvent, targetRowIndexes: [TargetRowModel])
-
-    func moveRow(event: FieldChangeEvent, targetRowIndexes: [TargetRowModel])
-
-    func deleteRow(event: FieldChangeEvent, targetRowIndexes: [TargetRowModel])
-
-    /// Notifies the conforming object when a field gains focus.
-    ///
-    /// - Parameter event: The `FieldEvent` object that represents the field event.
-    func onFocus(event: FieldEvent)
-    
-    /// Notifies the conforming object when an upload event occurs.
-    ///
-    /// - Parameter event: The `UploadEvent` object that represents the upload event.
-    func onUpload(event: UploadEvent)
 }
 
 public struct TargetRowModel {
