d76f1a83fd8ad0494e02594897c0cb72fd184c25
Vishnu <vishnubishnoi7@gmail.com>
2024-11-21 17:09:46 +0530

    Create Document editor

---
+++ Sources/JoyfillModel/DocumentEditor.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Vishnu Dutt on 21/11/24.
+//
+
+import Foundation
+
+public class DocumentEditor {
+    var document: JoyDoc
+
+    public init(document: JoyDoc) {
+        self.document = document
+    }
+
+
+}
